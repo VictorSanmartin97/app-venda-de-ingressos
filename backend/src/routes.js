6974e5655a6d3dc1af128d873da3fc2771db32ac
William Andrei Nascimento Silveira <william.silveira@pampa.compasso>
2020-05-20 20:54:34 -0300

    fix routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,12 @@
 require('dotenv').config();
 
-
 const express = require('express');
 const cliente_controller = require('./controllers/cliente_controller');
-
 const login_controller = require('./controllers/login_controller');
- 
 const auth = require('./midlleware/auth');
 
 const routes = express.Router();
-const jwt = require('jsonwebtoken');
+
 
 routes.get('/cliente', auth.authenticateToken , cliente_controller.index);//exemplo com midlleware
 routes.post('/cliente', cliente_controller.create);
