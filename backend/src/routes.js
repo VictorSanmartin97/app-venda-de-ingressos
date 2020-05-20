7d9bdeb3f4ab912247ed0d2c5ecec4934584bbe9
William Andrei Nascimento Silveira <william.silveira@pampa.compasso>
2020-05-19 23:15:22 -0300

    validação de jwt correções

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,29 +2,20 @@ require('dotenv').config();
 
 
 const express = require('express');
-<<<<<<< HEAD
 const cliente_controller = require('./controllers/cliente_controller');
 
 const login_controller = require('./controllers/login_controller');
  
-=======
->>>>>>> 0c1397e3780b201e3c90fc20982205544078655a
 
-const cliente_controller = require('./controllers/cliente_controller');
-const usuario_controller = require('./controllers/usuario_controller');
 const routes = express.Router();
 const jwt = require('jsonwebtoken');
 
-routes.get('/cliente', authenticateToken, cliente_controller.index);
+routes.get('/cliente', authenticateToken, cliente_controller.index);//exemplo com midlleware
 routes.post('/cliente', cliente_controller.create);
-<<<<<<< HEAD
 routes.post('/login', login_controller.login);
 
 
 module.exports = routes;
-=======
-routes.get('/usuario', usuario_controller.getByLogin);
->>>>>>> 0c1397e3780b201e3c90fc20982205544078655a
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
