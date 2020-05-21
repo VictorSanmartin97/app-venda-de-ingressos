e3d0779ec82ef12bb89f0d167cef8198d479be66
Gcunha88 <gustavofaccocunha@hotmail.com>
2020-05-20 21:02:23 -0300

    [ add ] Create Ingresso page

--- frontend/src/paginas/CadastrarIngresso/index.js
+++ frontend/src/paginas/CadastrarIngresso/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import logoImg from '../../imagens/logo-branco.png';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
+import './styles.css';
 library.add(fas);
 
 export default function Dashboard(){
+    const [selectDateIn, setSelectDateIn] = useState(new Date());
+    const [selectDateOut, setSelectDateOut] = useState(new Date());
     return (
         <div className="dashboard">
             <sidebar>
@@ -43,8 +48,48 @@ export default function Dashboard(){
                     <Link to="/"> <FontAwesomeIcon icon="sign-out-alt"/> Logout </Link>
 
                 </header>
-                <div class="conteudo">
-                    <p>Cadastrar Ingresso</p>
+                <div className="conteudo">
+                    <div className="box">
+                        <h1><FontAwesomeIcon icon="external-link-square-alt"/> Cadastrando seu Ingresso</h1>
+                        <p>Prencha as informações do seu ingresso.</p>
+                         <form>
+                            <div className="input-grupo">
+                                <input type="text" value="Nome do Ingresso" className="nomeIngresso" />
+                                <input type="text" value="Quantidade" className="qtd"/>
+                                <input type="text" value="Preço R$" className="preco"/>
+                            </div>
+                                <input type="text" value="Setor do Ingresso" className="setorIngresso"/>
+                                
+                                <div className="box-calendario">
+                                        <div className="calendario-inicio">
+                                            <p>Inicio das Vendas:</p>
+                                            <DatePicker
+                                            selected={selectDateIn}
+                                            onChange={date => setSelectDateIn(date)}
+                                            showTimeSelect
+                                            timeFormat="HH:mm"
+                                            timeIntervals={15}
+                                            timeCaption="time"
+                                            dateFormat="d MMMM, yyyy h:mm aa"
+                                            />
+                                        </div>
+                                        <div className="calendario-fim">
+                                            <p>Fim das Vendas:</p>
+                                            <DatePicker 
+                                            selected={selectDateOut}
+                                            onChange={date => setSelectDateOut(date)}
+                                            showTimeSelect
+                                            timeFormat="HH:mm"
+                                            timeIntervals={15}
+                                            timeCaption="time"
+                                            dateFormat="d MMMM, yyyy h:mm aa"
+                                            />
+                                        </div>   
+                                </div>
+                                <button type="submit" className="btn">Cadastrar Ingresso</button>
+                        </form>
+                         
+                    </div>      
                 </div>
             </main>
         </div>
