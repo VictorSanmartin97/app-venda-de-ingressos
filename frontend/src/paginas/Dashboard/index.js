8d1f8a0903cec39db6787a9fc1e7d93aca715da8
Gcunha88 <gustavofaccocunha@hotmail.com>
2020-06-14 22:10:24 -0300

    Relatorios e Dashboard

--- frontend/src/paginas/Dashboard/index.js
+++ frontend/src/paginas/Dashboard/index.js
@@ -4,6 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import logoImg from '../../imagens/logo-branco.png';
+import './styles.css';
 library.add(fas);
 
 export default function Dashboard(){
@@ -47,7 +48,38 @@ export default function Dashboard(){
 
                 </header>
                 <div class="conteudo">
-                    <p>Dashboard</p>
+                    <div className="central">
+                        <div className="box-prox-evento">
+                            <h1><FontAwesomeIcon icon="calendar-alt"/> Próximo Evento</h1>
+                            <br></br>
+                            <h2> Balada Hawaiana</h2>
+                            <p>Abaixo número de ingressos já vendidos.</p>
+                                <ul class="list-group">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                        Universitário
+                                        <span class="badge badge-primary badge-pill">97</span>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                        Pista 1º Lote
+                                        <span class="badge badge-primary badge-pill">28</span>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                        Pista 2º Lote
+                                        <span class="badge badge-primary badge-pill">0</span>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                        Camarote
+                                        <span class="badge badge-primary badge-pill">17</span>
+                                    </li>
+                                </ul>
+
+                        </div>
+                        <div className="box-ingressos">
+                            <h1><FontAwesomeIcon icon="external-link-square-alt"/> Seus ingressos mais vendidos.</h1>
+                            <br></br>
+                            <p>OBS:Criar uma listagem com os ingressos mais vendidos, pegando todos os vendidos em todos os eventos fazer tipo um ranking do mais vendido.</p>
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
